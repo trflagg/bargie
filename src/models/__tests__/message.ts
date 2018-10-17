4b7f2ce867f69008189d15a7b8dbf196317c5419
Taylor Flagg <trflagg@gmail.com>
2018-10-17 16:06:21 -0400

    Add Message model

---
+++ src/models/__tests__/message.ts
@@ -0,0 +1,16 @@
+import * as _ from 'lodash';
+import {MessageModel} from '../message';
+import {StoryModel} from '../story';
+
+describe('MessageModel', () => {
+  it('can make a new instance', async () => {
+    const story = new StoryModel();
+    const message = new MessageModel();
+    message.name = 'TESTMESSAGE';
+    message.text = 'This is the text of our message';
+    message.story = story._id;
+    message.compiledSource = _.template(message.text).source;
+    message.messagesLoaded = [];
+    await message.validate();
+  });
+});
---
+++ src/models/message.ts
@@ -0,0 +1,35 @@
+import * as _ from 'lodash';
+import {Document, Model, model, Schema, Types} from 'mongoose';
+
+export interface MessageInterface extends Document {
+  name: string;
+  text: string;
+  story: Types.ObjectId;
+  compiledSource: string;
+  messagesLoaded: Types.ObjectId[];
+}
+
+const MessageSchema = new Schema(
+    {
+      name: {
+        type: String,
+        minlength: 3,
+        maxlength: 255,
+        uppercase: true,
+        required: true,
+      },
+      text: String,
+      story: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Story',
+      },
+      compiledSource: String,
+      messagesLoaded: [Schema.Types.ObjectId],
+    },
+    {timestamps: true});
+
+MessageSchema.index({story: 1, name: 1}, {unique: true});
+
+export const MessageModel: Model<MessageInterface> =
+    model<MessageInterface>('Message', MessageSchema);
